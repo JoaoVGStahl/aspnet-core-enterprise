6c714704019167e5b497e8ed3b5a42a4f0618839
João Girardi <jvitorstahl@hotmail.com>
2022-02-21 22:09:16 -0300

    add https comunication for all APIs

--- src/api gateways/NSE.Bff.Compras/Controllers/CarrinhoController.cs
+++ src/api gateways/NSE.Bff.Compras/Controllers/CarrinhoController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NSE.Bff.Compras.Models;
 using NSE.Bff.Compras.Services;
+using NSE.Bff.Compras.Services.gRPC;
 using NSE.WebAPI.Core.Controllers;
 
 namespace NSE.Bff.Compras.Controllers
@@ -13,15 +14,18 @@ namespace NSE.Bff.Compras.Controllers
     public class CarrinhoController : MainController
     {
         private readonly ICarrinhoService _carrinhoService;
+        private readonly ICarrinhoGrpcService _carrinhoGrpcService;
         private readonly ICatalogoService _catalogoService;
         private readonly IPedidoService _pedidoService;
 
         public CarrinhoController(
-            ICarrinhoService carrinhoService, 
+            ICarrinhoService carrinhoService,
+            ICarrinhoGrpcService carrinhoGrpcService,
             ICatalogoService catalogoService, 
             IPedidoService pedidoService)
         {
             _carrinhoService = carrinhoService;
+            _carrinhoGrpcService = carrinhoGrpcService;
             _catalogoService = catalogoService;
             _pedidoService = pedidoService;
         }
--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -11,6 +11,7 @@ using NSE.WebAPI.Core.Usuario;
 using Polly;
 using Polly.Extensions.Http;
 using Polly.Retry;
+using NSE.WebAPI.Core.Extensions;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -28,24 +29,28 @@ namespace NSE.WebApp.MVC.Configuration
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>()
                 .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AllowSelfSignedCertificate()
                 .AddTransientHttpErrorPolicy(
                     p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
             services.AddHttpClient<ICatalogoService, CatalogoService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
                 .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AllowSelfSignedCertificate()
                 .AddTransientHttpErrorPolicy(
                     p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
             services.AddHttpClient<IComprasBffService, ComprasBffService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
                 .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AllowSelfSignedCertificate()
                 .AddTransientHttpErrorPolicy(
                     p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
             services.AddHttpClient<IClienteService, ClienteService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
                 .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AllowSelfSignedCertificate()
                 .AddTransientHttpErrorPolicy(
                     p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
